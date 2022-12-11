df47df62db7f96426dab1fb3846a66b0b1990097
francolivelli <franco.livelli@gmail.com>
2022-12-11 14:40:07 -0300

    connect to db

--- api/app.js
+++ api/app.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const morgan = require("morgan")
-
 const app = express();
+app.use(express.json());
 
-app.use(express.json())
-app.use(morgan("tiny"))
+const morgan = require("morgan");
+app.use(morgan("tiny"));
 
-require("dotenv").config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "../.env" });
+}
 
-app.listen(process.env.PORT, ()=>{
-    console.log("Server running on port", process.env.PORT)
-})
+const PORT = process.env.PORT || 3000;
+
+const db = require("./config/db");
+
+db.sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server is listening on port", PORT);
+    });
+  })
+  .catch(console.error);
